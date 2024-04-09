b7d30eb2a7f5deef7840d7f2c3b1b00878660ff3
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2024-04-09 00:29:11 -0400

    nav bar and routes added

---
+++ components/ui/nav/nav.tsx
@@ -0,0 +1,122 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+// import { Icons } from "@/components/icons"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/nav/navigation-menu"
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Text Generation",
+    href: "/text-generation",
+    description:
+      "OpenAI's text generation models have been trained to understand natural language, code, and images.",
+  },
+  {
+    title: "Image Generator",
+    href: "/image-generator",
+    description:
+      "Generate or manipulate images with DALL·E from OpenAI",
+  },
+  {
+    title: "Open AI Vision",
+    href: "/vision",
+    description:
+      "Use GPT-4 to understand images.",
+  },
+  {
+    title: "Text-to-Speech",
+    href: "/text-to-speech",
+    description: "Turn text into lifelike spoken audio.",
+  },
+]
+
+export function NavigationMenuAi() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    {/* <Icons.logo className="h-6 w-6" /> */}
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      AI Chatbot
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautiful and simply designed components built with Radix UI and
+                      Tailwind CSS for this AI Chatbot.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/introduction" title="Introduction">
+                Get introduced to OpenAi and how to use it to you're advantage.
+              </ListItem>
+              <ListItem href="/documentation" title="Documentation">
+                Take a look at the OpenAI Documention.
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Ai Tools</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
