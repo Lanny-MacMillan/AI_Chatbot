0114e94324695104e238dd9c9f20c04dc1592995
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2024-04-18 23:46:15 -0400

    added new themes to tailwind config

--- components/ui/nav/nav.tsx
+++ components/ui/nav/nav.tsx
@@ -54,7 +54,6 @@ export function NavigationMenuAi() {
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
-                    {/* <Icons.logo className="h-6 w-6" /> */}
                     <div className="mb-2 mt-4 text-lg font-medium">
                       Ai Toolbox
                     </div>
