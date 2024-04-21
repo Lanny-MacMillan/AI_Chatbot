eb9ddfa35b459dd9927156b3a6416cacb6fcc60c
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2024-04-21 15:56:48 -0400

    more styling changes, mainly color, theme and outline gradients

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,32 @@ import { NavigationMenuAi } from '@/components/ui/nav/nav'
 export const runtime = 'edge'
 
 export default function Home() {
+  // framer motion text animation
+  //      "Transform your
+  // approach with AI-Powered Solutions"
+  
+  // custom SVG below
 
+  // <motion.div
+  // style={{ marginTop: '1em'}}
+  // initial={{ y: -20, opacity: 0 }}
+  // animate={{ y: 0, opacity: 1 }}
+  // exit={{ y: 20, opacity: 0}}
+  // transition={{ ease: "easeInOut", duration: 1 }}>{animatedContent}</motion.div>
   return (
-    <div className='bg-gradient-to-b from-[#ffffff] to-[#5be9b9]' >
+    <div className='bg-gradient-to-b from-[#ffffff] to-[#5be9b9] h-screen' >
 
       <NavigationMenuAi/>
-      <section className='text-zinc-700'>
-        <div className="container flex h-screen flex-col items-center justify-center">
-          <h1
-            id='text'
-            className="bg-gradient-to-r from-custom-purple-600 to-custom-magenta-300 inline-block text-transparent bg-clip-text text-9xl font-customBlack">Ai Toolbox</h1>
+      <section className='text-zinc-700 border-custom-purple-600'>
+        <div className="container mx-auto flex flex-col border-custom-purple-600">
+          <div className="flex mt-40">
+            <h1 id='text'
+              className="bg-gradient-to-r from-custom-purple-600 to-custom-magenta-300 inline-block text-transparent bg-clip-text text-9xl font-customBlack mt-32 mb-40">Ai Toolbox</h1>
+          </div>
+          <div className='flex mt-28'>
+            <h1 id='text'
+            className="text-7xl text-center font-customBlack">Transform Your Approach With <span className="bg-gradient-to-r from-custom-purple-600 to-custom-magenta-300 inline-block text-transparent bg-clip-text font-customBlack">Ai-Powered</span> Solutions</h1>
+          </div>
         </div>
       </section>
     </div>
