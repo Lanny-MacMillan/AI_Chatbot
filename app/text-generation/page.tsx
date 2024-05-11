e9382523b261b4ea95446576480c958cf3bc41bb
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2024-05-11 16:11:24 -0400

    tried to tackle mobile issue with css

--- app/text-generation/page.tsx
+++ app/text-generation/page.tsx
@@ -1,15 +1,16 @@
-import Chat from '@/components/chat'
-import { NavigationMenuAi } from '@/components/ui/nav/nav'
+import Chat from '@/components/chat';
+import { NavigationMenuAi } from '@/components/ui/nav/nav';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 export default function Introduction() {
-
-
   return (
-    <div className='bg-gradient-to-b from-[#ffffff] to-[#5be9b9] h-dvh max-h-dvh overflow-hidden' >
-      <NavigationMenuAi/>
+    <div
+      id="heightContainer"
+      className="bg-gradient-to-b from-[#ffffff] to-[#5be9b9] h-dvh max-h-dvh overflow-hidden"
+    >
+      <NavigationMenuAi />
       <Chat />
     </div>
-  ) 
+  );
 }
