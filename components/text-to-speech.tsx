8ca3f35162d4cbe0a6e6215975abe6ad228e99e8
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2024-05-13 00:14:17 -0400

    copy change in vision

--- components/text-to-speech.tsx
+++ components/text-to-speech.tsx
@@ -1,166 +1,183 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
-import { motion, AnimatePresence } from 'framer-motion'
-import { SendHorizontalIcon } from 'lucide-react'
-import Wavesurfer from './ui/wavesurfer/wavesurfer';
-import { Input, SelectAudio, SelectModel, SelectVoice } from '@/components/ui';
+import { motion, AnimatePresence } from "framer-motion";
+import { SendHorizontalIcon } from "lucide-react";
+import Wavesurfer from "./ui/wavesurfer/wavesurfer";
+import { Input, SelectAudio, SelectModel, SelectVoice } from "@/components/ui";
 
 export default function Speech() {
-  const [input, setInput] = useState<string>('');
-  const [audio, setAudio] = useState<any>('');
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [voice, setVoice] = useState<string>('alloy');
-  const [model, setModel] = useState<string>('tts-1');
-  const [downloadAudio, setDownloadAudio] = useState<string>('false');
-  const [pause, setPause] = useState<boolean>(false);
-  const [hover, setHover] = useState<boolean>(false);
+	const [input, setInput] = useState<string>("");
+	const [audio, setAudio] = useState<any>("");
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [voice, setVoice] = useState<string>("alloy");
+	const [model, setModel] = useState<string>("tts-1");
+	const [downloadAudio, setDownloadAudio] = useState<string>("false");
+	const [pause, setPause] = useState<boolean>(false);
+	const [hover, setHover] = useState<boolean>(false);
 
-  const standardClass =
-	"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 absolute right-1 top-1 h-9 w-12 bg-custom-purple-100 text-secondary-foreground h-10 w-10";
+	const standardClass =
+		"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 absolute right-1 top-1 h-9 w-12 bg-custom-purple-100 text-secondary-foreground h-10 w-10";
 
-  const hoverClass =
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 absolute right-1 top-1 h-9 w-12 bg-custom-teal-100 text-secondary-foreground h-10 w-10";
-  
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const newValue = e.currentTarget.value;
-    setInput(newValue)
-  }
+	const hoverClass =
+		"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 absolute right-1 top-1 h-9 w-12 bg-custom-teal-100 text-secondary-foreground h-10 w-10";
 
-  const handleSubmitDownload = async (e: any) => {
-    e.preventDefault();
-    setAudio('')
+	const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+		const newValue = e.currentTarget.value;
+		setInput(newValue);
+	};
 
-    try {
-      setIsLoading(true)
+	const handleSubmitDownload = async (e: any) => {
+		e.preventDefault();
+		setAudio("");
 
-      const response = await fetch("/api/textToSpeech", {
-        body: JSON.stringify({ input, voice, model }),
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      
-      if (!response.ok) throw new Error("Error generating audio");
+		try {
+			setIsLoading(true);
 
-      toast.success("Audio generated successfully!");
+			const response = await fetch("/api/textToSpeech", {
+				body: JSON.stringify({ input, voice, model }),
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
 
-      const blob = await response.blob();
-      const downloadUrl = URL.createObjectURL(blob);
-      
-      const link = document.createElement("a");
-      link.href = downloadUrl;
-      link.setAttribute("download", "vision.mp3");
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      
-    } catch (error) {
-      console.error(error);
+			if (!response.ok) throw new Error("Error generating audio");
 
-    } finally {
-      setIsLoading(false)
-    }
-  }
+			toast.success("Audio generated successfully!");
 
-  const handleSubmitAudio = async (e: any) => {
-    e.preventDefault();
-    setAudio('')
-    try {
-      setIsLoading(true)
+			const blob = await response.blob();
+			const downloadUrl = URL.createObjectURL(blob);
 
-      const response = await fetch("/api/textToSpeech", {
-        body: JSON.stringify({ input, voice, model }),
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      
-      if (!response.ok) throw new Error("Error generating audio");
+			const link = document.createElement("a");
+			link.href = downloadUrl;
+			link.setAttribute("download", "vision.mp3");
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-      toast.success("Audio generated successfully!");
+	const handleSubmitAudio = async (e: any) => {
+		e.preventDefault();
+		setAudio("");
+		try {
+			setIsLoading(true);
 
-      const blob = await response.blob();
-      const downloadUrl = URL.createObjectURL(blob);
-      setAudio(downloadUrl);
+			const response = await fetch("/api/textToSpeech", {
+				body: JSON.stringify({ input, voice, model }),
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
 
-    } catch (error) {
-      console.error(error);
+			if (!response.ok) throw new Error("Error generating audio");
 
-    } finally {
-      setIsLoading(false)
-    }
-  }
+			toast.success("Audio generated successfully!");
 
-  return (
-    <AnimatePresence>
+			const blob = await response.blob();
+			const downloadUrl = URL.createObjectURL(blob);
+			setAudio(downloadUrl);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-      <div className=" container flex w-screen flex-col items-center justify-around">
-      <div className="flex flex-row ">
-        <motion.p
-          className="bg-gradient-to-r from-custom-purple-600 to-custom-magenta-300 inline-block text-transparent bg-clip-text text-5xl font-customBlack  text-center mt-16 mb-16"
-          initial={{ y: -20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 20, opacity: 0}}
-          transition={{ type: "spring", bounce: .7 }}
-        >{'Text '}
-          </motion.p>
-          
-          <motion.p
-            className="bg-gradient-to-r from-custom-magenta-300 to-custom-magenta-300 inline-block text-transparent bg-clip-text text-5xl font-customBlack  text-center mt-16 mb-16 mr-4 ml-4"
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 20, opacity: 0}}
-            transition={{ delay: .5, type: "spring", bounce: .7 }}
-          > to
-          </motion.p>
-          <motion.p
-            className="bg-gradient-to-r from-custom-magenta-300 to-custom-purple-500 inline-block text-transparent bg-clip-text text-5xl font-customBlack  text-center mt-16 mb-16"
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 20, opacity: 0}}
-            transition={{ delay: 1, type: "spring", bounce: .7 }}
-          > Speech
-            </motion.p>
-      </div>
-        <div className="w-full max-w-lg ">
+	return (
+		<AnimatePresence>
+			<div className=" container flex w-screen flex-col items-center justify-around">
+				<div className="flex flex-row ">
+					<motion.p
+						className="bg-gradient-to-r from-custom-purple-600 to-custom-magenta-300 inline-block text-transparent bg-clip-text text-5xl font-customBlack  text-center mt-16 mb-16"
+						initial={{ y: -20, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: 20, opacity: 0 }}
+						transition={{ type: "spring", bounce: 0.7 }}
+					>
+						{"Text "}
+					</motion.p>
 
-        {audio ? (
-          <div className='lg:mt-16'>
-            <Wavesurfer className='lg:mt-16' audio={audio} pause={pause} setPause={setPause} />
-          </div>
-          ) : <div className='h-44 lg:mt-16'/>}
-          
-          <form onSubmit={downloadAudio === "true" ? handleSubmitDownload : handleSubmitAudio} className='relative rounded-md w-full max-w-lg mt-16 shadow-2xl'>
-            <Input
-              name='message'
-              onChange={onChange}
-              placeholder='What would you like me to say?...'
-            />
-            <motion.button
-              type='submit'
-              disabled={isLoading || input.length === 0}
+					<motion.p
+						className="bg-gradient-to-r from-custom-magenta-300 to-custom-magenta-300 inline-block text-transparent bg-clip-text text-5xl font-customBlack  text-center mt-16 mb-16 mr-4 ml-4"
+						initial={{ y: -20, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: 20, opacity: 0 }}
+						transition={{ delay: 0.5, type: "spring", bounce: 0.7 }}
+					>
+						{" "}
+						to
+					</motion.p>
+					<motion.p
+						className="bg-gradient-to-r from-custom-magenta-300 to-custom-purple-500 inline-block text-transparent bg-clip-text text-5xl font-customBlack  text-center mt-16 mb-16"
+						initial={{ y: -20, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: 20, opacity: 0 }}
+						transition={{ delay: 1, type: "spring", bounce: 0.7 }}
+					>
+						{" "}
+						Speech
+					</motion.p>
+				</div>
+				<div className="w-full max-w-lg ">
+					{audio ? (
+						<div className="lg:mt-16">
+							<Wavesurfer
+								className="lg:mt-16"
+								audio={audio}
+								pause={pause}
+								setPause={setPause}
+							/>
+						</div>
+					) : (
+						<div className="h-44 lg:mt-16" />
+					)}
 
-              className={ hover ? hoverClass : standardClass }
-              whileHover={{ scale: 1.4 }}
-              transition={{ tpe: "spring", stiffness: 300 }}
-              whileTap={{ scale: 1.7 }}
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
-            >
-              <SendHorizontalIcon className={ hover ? 'h-5 w-5 text-custom-purple-500'  : 'h-5 w-5 text-custom-teal-500'}/>
-            </motion.button>
-          </form>
-          <div className=" flex flex-row justify-between mt-8"> 
-              <SelectVoice setVoice={setVoice} />
-              <SelectModel setModel={setModel} />
-              <SelectAudio setDownloadAudio={setDownloadAudio} /> 
-          </div>         
-        </div>
-      </div>
-      </AnimatePresence>
-
-  );
+					<form
+						onSubmit={
+							downloadAudio === "true"
+								? handleSubmitDownload
+								: handleSubmitAudio
+						}
+						className="relative rounded-md w-full max-w-lg mt-16 shadow-2xl"
+					>
+						<Input
+							name="message"
+							onChange={onChange}
+							placeholder="What would you like me to say?..."
+						/>
+						<motion.button
+							type="submit"
+							disabled={isLoading || input.length === 0}
+							className={hover ? hoverClass : standardClass}
+							whileHover={{ scale: 1.4 }}
+							transition={{ tpe: "spring", stiffness: 300 }}
+							whileTap={{ scale: 1.7 }}
+							onMouseEnter={() => setHover(true)}
+							onMouseLeave={() => setHover(false)}
+						>
+							<SendHorizontalIcon
+								className={
+									hover
+										? "h-5 w-5 text-custom-purple-500"
+										: "h-5 w-5 text-custom-teal-500"
+								}
+							/>
+						</motion.button>
+					</form>
+					<div className=" flex flex-row justify-between mt-8 mb-4">
+						<SelectVoice setVoice={setVoice} />
+						<SelectModel setModel={setModel} />
+						<SelectAudio setDownloadAudio={setDownloadAudio} />
+					</div>
+				</div>
+			</div>
+		</AnimatePresence>
+	);
 }
