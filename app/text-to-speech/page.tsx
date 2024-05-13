2f759bed54cc2c95734a140f069d33227952b98f
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2024-05-12 23:42:03 -0400

    new height container put on each page, removed unused pages

--- app/text-to-speech/page.tsx
+++ app/text-to-speech/page.tsx
@@ -1,14 +1,16 @@
-import Speech from '@/components/text-to-speech'
-import { NavigationMenuAi } from '@/components/ui/nav/nav'
+import Speech from '@/components/text-to-speech';
+import { NavigationMenuAi } from '@/components/ui/nav/nav';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 export default function Introduction() {
-
   return (
-    <div className='bg-gradient-to-b from-[#ffffff] to-[#5be9b9] h-dvh max-h-dvh overflow-hidden' >
-      <NavigationMenuAi/>
+    <div
+      id="heightContainer"
+      className="bg-gradient-to-b from-[#ffffff] to-[#5be9b9] overflow-hidden"
+    >
+      <NavigationMenuAi />
       <Speech />
     </div>
-  ) 
+  );
 }
