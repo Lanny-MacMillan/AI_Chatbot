047aebf312e68444f90939e1eb137e31035b9d09
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2025-01-08 17:08:00 -0500

    fixed gtags

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,30 +20,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        {/* Google Tag Manager */}
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-55XB6PC1CC"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','G-55XB6PC1CC');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-55XB6PC1CC');
             `,
           }}
         />
       </Head>
       <UserProvider>
         <body className={inter.className}>
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=G-55XB6PC1CC"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
           {children}
           <Toaster />
         </body>
